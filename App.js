c431a3b491b65146e51318950bb7f079ef11b081
Owen VO <hlv.owen@gmail.com>
2024-02-04 09:37:55 +0100

    fonts added

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+//#region Expo imports
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+//#endregion
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    regular: require("./src/assets/fonts/Montserrat-Regular.ttf"),
+    medium: require("./src/assets/fonts/Montserrat-Medium.ttf"),
+    bold: require("./src/assets/fonts/Montserrat-Bold.ttf"),
+    light: require("./src/assets/fonts/Montserrat-Light.ttf"),
+    xtrabold: require("./src/assets/fonts/Montserrat-ExtraBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
